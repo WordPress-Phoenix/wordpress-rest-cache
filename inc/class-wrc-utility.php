07d046f64b8c422e80f2422b0f7f11051f911ae0
Seth Carstens <seth.carstens@gmail.com>
2016-10-11 13:43:08 -0700

    upgrade utility

--- inc/class-wrc-utility.php
+++ inc/class-wrc-utility.php
@@ -8,14 +8,19 @@
  */
 class WRC_Utility {
 
+	public static function clear_cache_by( $column, $value ) {
+		global $wpdb;
+
+		return $wpdb->delete( $wpdb->base_prefix . WP_Rest_Cache::$table, array( $column => $value ) );
+	}
+
 	/**
 	 * Deletes all github api calls from the rest cache to operate with afragen/github-updater plugin
 	 *
 	 * @return false|int number of deleted cache rows
 	 */
 	public static function clear_ghu_cache() {
-		global $wpdb;
-		return $wpdb->delete( 'wp_rest_cache', array( 'rest_domain' => 'https://api.github.com' ) );
+		self::clear_cache_by( 'rest_domain', 'https://api.github.com' );
 	}
-	
+
 }
