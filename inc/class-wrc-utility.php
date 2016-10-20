23019d14b180badb32cbbe70f9c157ba0cefbb44
Andy Fragen <andy@thefragens.com>
2016-10-20 12:16:30 -0700

    add filter to allow additional key/values for clearing
    Adds filter
    Sets defaults of 'api.github.com' and 'GitHub Updater'
       'GitHub Updater' appears after in rest_args, this can be needed for incidental enterprise git servers.
    Adjusts clear_cache_by to accept array as parameter

--- inc/class-wrc-utility.php
+++ inc/class-wrc-utility.php
@@ -11,15 +11,16 @@ class WRC_Utility {
 	/**
 	 * Utility to easily delete cache by exact column value
 	 *
-	 * @param $column
-	 * @param $value
+	 * @param $clear_cache
 	 *
 	 * @return false|int
 	 */
-	public static function clear_cache_by( $column, $value ) {
+	public static function clear_cache_by( $clear_cache ) {
 		global $wpdb;
 
-		return $wpdb->delete( $wpdb->base_prefix . WP_Rest_Cache::$table, array( $column => $value ) );
+		array_filter( $clear_cache, function( $args ) use ( $wpdb ) {
+			return $wpdb->delete( $wpdb->base_prefix . WP_Rest_Cache::$table, $args );
+		} );
 	}
 
 	/**
@@ -28,7 +29,23 @@ class WRC_Utility {
 	 * @return int number of deleted cache rows
 	 */
 	public static function clear_ghu_cache() {
-		return self::clear_cache_by( 'rest_domain', 'https://api.github.com' );
+		$clear_cache = array(
+			array( 'rest_domain' => 'https://api.github.com' ),
+			array( 'rest_args' => 'GitHub Updater' ),
+		);
+
+		/**
+		 * Filters array of arrays containing key/value pairs of column/value pairs
+		 * for removal from wp_rest_cache table.
+		 *
+		 * @param array $clear_cache Array of arrays containing key/value pairs for removal.
+		 *                           Default contains GitHub and GitHub Updater values.
+		 *
+		 * @return array $clear_cache Merged array.
+		 */
+		$clear_cache = apply_filters( 'wp_rest_cache_clear_extra', $clear_cache );
+
+		return self::clear_cache_by( $clear_cache );
 	}
 
 }
