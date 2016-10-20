99932e5bd71950ea61c422085bc8ff9ccb9ad609
Seth Carstens <seth.carstens@gmail.com>
2016-10-19 22:09:18 -0700

    scoping changes and ghu filter mods

--- inc/class-wrc-filters.php
+++ inc/class-wrc-filters.php
@@ -27,7 +27,12 @@ class WRC_Filters {
 	 * Admin only filters are registered
 	 */
 	static function admin_filters() {
+		// hook into refresh transients and clear cache at the same time
+		add_action( 'ghu_refresh_transients', array( 'WRC_Utility', 'clear_ghu_cache') );
+		// disable using transients as it is basically double caching
 		add_filter( 'ghu_use_remote_call_transients', '__return_false' );
+		// bypass update logic so ghu always reaches out to rest_cache for data
+		add_filter('ghu_always_fetch_update', '__return_true');
 	}
 
 	/**
