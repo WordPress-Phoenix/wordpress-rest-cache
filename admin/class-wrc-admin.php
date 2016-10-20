4879bbfdd7f13abb9f0f11791b5f4d742d164be3
Seth Carstens <seth.carstens@gmail.com>
2016-10-20 00:27:38 -0700

    beta admin page enhancements

--- admin/class-wrc-admin.php
+++ admin/class-wrc-admin.php
@@ -52,83 +52,132 @@ if ( ! class_exists( 'WRC_Admin' ) ) {
 			);
 		}
 
-		static function options_page() {
+		static function process_request(){
+			$updated_div = '<div class="updated">';
+			$error_div = '<div class="error">';
 
 			if ( ! empty( $_REQUEST['wrc-entry-delete'] ) ) {
 				$deleted = WRC_Utility::clear_cache_by( 'rest_md5', $_REQUEST['wrc-entry-delete'] );
 
 				if ( false !== $deleted ) {
-					echo '<div class="updated"><p>The following entry was deleted:';
-					echo '<pre>';
-					var_export( $deleted );
-					echo '</pre>';
-					echo '</p></div>';
-
+					$msg  = 'The following entry was deleted:<br /><pre>' . var_export( $deleted, true ) . '</pre>';
+					return static::get_action_message( 'update', $msg );
 				} else {
-					echo '<div class="error"><p>Oops, looks like there was an issue deleting that entry.</p></div>';
+					$msg = 'Oops, looks like there was an issue deleting that entry.';
+					return static::get_action_message( 'update', $msg );
 				}
-
+				return $msg;
 			}
 
+			// Validate action submitted
+			if( empty( $_REQUEST['wrc-action'] ) ) {
+				return '';
+			}
+
+			// If action submitted validate nonce
+			if( ! wp_verify_nonce( $_REQUEST[static::$utilities_id], $_REQUEST['wrc-action'] ) ) {
+				return static::get_action_message('error', 'Error: failed nonce validation.');
+			}
+
+			// Assess action since validation passed
+			switch ( $_REQUEST['wrc-action'] ) {
+				case 'clear-ghu-cache':
+					die('works, about to delete ghu cache');
+					$results = WRC_Utility::clear_ghu_cache();
+					if( $results ) {
+						return static::get_action_message( 'updated', 'GHU Rest Cache Cleared.' );
+					} else {
+						return static::get_action_message( 'updated', 'Action returned an error.' );
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		static function options_page() {
+			if ( ! empty( $_REQUEST) ) {
+				echo static::process_request();
+			}
 			?>
 			<div class="wrap">
 			<h1>WP REST Cache Utilities</h1>
-
 			<div id="<?php echo static::$utilities_id; ?>">
-				<form id="wrc-util-unused" method="POST" action="" class="card" style="max-width: 100%;">
+				<?php static::build_query_one_form(); ?>
+				<?php static::build_query_two_form(); ?>
+				<?php static::build_cache_clear_form(); ?>
+
+			</div>
+			<?php
+		}
+		static function build_cache_clear_form(){
+			$action = 'clear-ghu-cache';
+			?>
+			<form id="wrc-cache-clear-form" method="POST" action="" class="card" style="max-width: 100%;">
+			<p><strong>Delete GHU API Cache:</strong></p>
+			<?php wp_nonce_field( $action, static::$utilities_id ); ?>
+			<input type="hidden" name="wrc-action" value="clear-ghu-cache" />
+			<input type="submit" id="wrc-submit" value="Run" class="button-primary">
+			</form>
+			<?php
+		}
+
+		static function get_action_message( $type, $content ) {
+			$msg  = '<div class="'.$type.'">';
+			$msg .= '<p>' . $content . '</p>';
+			$msg .= '</div>';
+
+			return apply_filters('wrc_admin_build_page_action_message', $msg, $type, $content);
+		}
+
+
+		static function build_query_one_form(){
+			?>
+			<form id="wrc-util-unused" method="POST" action="" class="card" style="max-width: 100%;">
 					<p><strong>Check a subset of results based on last request date:</strong></p>
 					<?php wp_nonce_field( static::$utilities_id, 'wrc-util-unused' ); ?>
-					<label for="wrc-unused-num">Max # of rows to return: </label>
-					<br><input type="number" value="50"
-					           name="wrc-unused-num"
-					           id="wrc-unused-num"/>
-					<br><label for="wrc-days-ago">Check for results older than # of days: </label>
-					<br><input type="number" value="30"
-					           name="wrc-days-ago"
-					           id="wrc-days-ago"/>
-					<p class="description">Limit is querying the DB, it is recommended to keep the max rows number as
-						small as possible.</p>
-					<input type="submit" id="wrc-unused-submit" value="Run" class="button-primary">
-					<br>
-					<div class="results">
-
-						<?php
-						if (
-							! empty( $_REQUEST['wrc-util-unused'] )
-							&& wp_verify_nonce( $_REQUEST['wrc-util-unused'], static::$utilities_id )
-							&& ! empty( $_REQUEST['wrc-unused-num'] )
-						) {
-							$old_items = static::check_old_requests( $_REQUEST['wrc-unused-num'] );
-
-
-							echo '<hr><table style="width: 100%; text-align: left;"><thead>';
-							echo '<tr><th>Count</th><th>REST Call Domain</th></tr>';
-							echo '</thead><tbody>';
-
-							if ( ! empty( $old_items ) ) {
-								foreach ( $old_items as $old_item ) {
-									echo '<tr>';
-									echo '<td>' . $old_item['count'] . '</td>';
-									echo '<td>' . $old_item['rest_domain'] . '</td>';
-									echo '</tr>';
-								}
-							} else {
-								echo '<tr>';
-								echo '<td colspan="2"><p><em>';
-								echo 'There are no items with a last request date of more than ' . (int) $_REQUEST['wrc-days-ago'] . ' day(s) ago';
-								echo '</em></p></td>';
-								echo '</tr>';
+				<label for="wrc-unused-num">Max # of rows to return: </label>
+				<br><input type="number" value="50"
+				           name="wrc-unused-num"
+				           id="wrc-unused-num"/>
+				<br><label for="wrc-days-ago">Check for results older than # of days: </label>
+				<br><input type="number" value="30"
+				           name="wrc-days-ago"
+				           id="wrc-days-ago"/>
+				<p class="description">Limit is querying the DB, it is recommended to keep the max rows number as
+					small as possible.</p>
+				<input type="submit" id="wrc-unused-submit" value="Run" class="button-primary">
+				<br>
+				<div class="results">
+					<?php
+					if (
+						! empty( $_REQUEST['wrc-util-unused'] )
+						&& wp_verify_nonce( $_REQUEST['wrc-util-unused'], static::$utilities_id )
+						&& ! empty( $_REQUEST['wrc-unused-num'] )
+					) {
+						$old_items = static::check_old_requests( $_REQUEST['wrc-unused-num'] );
+						echo '<hr>';
+						$csv = '"REST Call Domain","md5"' . PHP_EOL;
+						if ( ! empty( $old_items ) ) {
+							foreach ( $old_items as $old_item ) {
+								$csv .= $old_item['count'] . ',';
+								$csv .= $old_item['rest_domain'] . PHP_EOL;
 							}
-
-							echo '</tbody></table>';
-
+							echo static::csvToTable($csv);
+						} else {
+							echo '<p>There are no items with a last request date of more than ' . (int) $_REQUEST['wrc-days-ago'] . ' day(s) ago</p>';
 						}
-						?>
-
-					</div>
+					}
+					?>
+				</div>
 				</form>
+				<?php
+		}
 
-				<form id="wrc-util-search" method="POST" action="" class="card" style="max-width: 100%;">
+
+		static function build_query_two_form(){
+			?>
+							<form id="wrc-util-search" method="POST" action="" class="card" style="max-width: 100%;">
 					<p><strong>Search for a specific request:</strong></p>
 					<?php wp_nonce_field( static::$utilities_id, 'wrc-util-search' ); ?>
 					<label for="wrc-limit-num">Max # of rows to return: </label>
@@ -149,7 +198,7 @@ if ( ! class_exists( 'WRC_Admin' ) ) {
 					<p><em>Both the Domain and Path fields are searched on an EQUALS basis to help deal with DB search
 							performance.</em></p>
 					<input type="submit" id="wrc-util-search-submit" value="Run" class="button-primary">
-					<br>
+					<br />
 					<div class="results">
 
 						<?php
@@ -168,43 +217,26 @@ if ( ! class_exists( 'WRC_Admin' ) ) {
 							} else {
 								$admin_url = admin_url( 'tools.php?page=' . self::$admin_page_slug );
 							}
-
-							echo '<hr><table style="width: 100%; text-align: left;"><thead>';
-							echo '<tr><th>REST Call Domain</th><th>REST Call Path</th><th></th><th>md5</th></tr>';
-							echo '</thead><tbody>';
+							echo '<hr>';
+							$csv = '"REST Call Domain","REST Call Path","md5","action"' . PHP_EOL;
 
 							if ( ! empty( $returned_items ) ) {
 								foreach ( $returned_items as $item ) {
-									echo '<tr>';
-									echo '<td>' . $item['rest_domain'] . '</td>';
-									echo '<td>' . $item['rest_path'] . '</td>';
-									echo '<td><form method="post" action="" id="delete_' . $item['rest_md5'] . '"><button type="submit" name="wrc-entry-delete" value="' . $item['rest_md5'] . '">Delete</button></form></td>';
-									echo '<td>' . $item['rest_md5'] . '</td>';
-									echo '</tr>';
+									$csv .= $item['rest_domain'] . ',';
+									$csv .= $item['rest_path'] . ',';
+									$csv .= $item['rest_md5'] . ',';
+									$csv .= '{{action_delete__' . $item['rest_md5'] . '}}' . PHP_EOL;
 								}
+								echo static::csvToTable($csv);
 							} else {
-								echo '<tr>';
-								echo '<td colspan="4"><p><em>';
 								echo 'There are no items that match your search for <em>' . $searched_domain . $searched_path . '</em>';
-								echo '</em></p></td>';
-								echo '</tr>';
 							}
-
-							echo '</tbody></table>';
-
 						}
 						?>
 
 					</div>
 				</form>
-				<div style="margin-top: 40px;">
-					<?php
-					if ( ! empty( $_REQUEST['wrc-phpinfo'] ) ) {
-						phpinfo();
-					}
-					?>
-				</div>
-			</div>
+				<br />
 			<?php
 		}
 
@@ -277,6 +309,64 @@ if ( ! class_exists( 'WRC_Admin' ) ) {
 			return false;
 		}
 
-	} // END class
 
+		/**
+		 * Fancy table printer
+		 *
+         * @param $csv_content
+         *
+		 * @return string
+        */
+		public static function csvToTable( $csv_content ) {
+			if( empty($csv_content) || ! stristr($csv_content, PHP_EOL ) ) {
+				return $csv_content;
+			}
+			$table  = '<table style="width: 100%; text-align: left;">';
+			// convert csv into array
+			$rows  = str_getcsv( $csv_content, "\n" );
+
+			// pull first row off and build table header
+			$table .= '<thead><tr>';
+			$header_row = array_shift( $rows );
+			$cells = str_getcsv( $header_row );
+			foreach ( $cells as &$cell ) {
+				$table .= "<th>$cell</th>";
+			}
+			$table .= '</thead>';
+
+			//build table body data
+			$table .= '<tbody>';
+			foreach ( $rows as &$row ) {
+				$table .= "<tr>";
+				$cells = str_getcsv( $row );
+				foreach ( $cells as &$cell ) {
+					if( substr( $cell, 0, 2 ) == '{{' ) {
+						$cell = static::do_cell_shortcode($cell);
+					}
+					$table .= "<td>$cell</td>";
+				}
+				$table .= "</tr>";
+			}
+			$table .= "</tbody></table>";
+
+			return $table;
+		}
+
+		public static function do_cell_shortcode( $shortcode ){
+			$shortcode = trim( $shortcode, '{}' );
+			$data = explode('__', $shortcode);
+			$action = array_shift($data);
+			switch ( $action ) {
+				case 'action_delete':
+					if( empty($data[0]) ) {
+						return '';
+					}
+					$out  = '<form method="post" action="" id="delete_' . $data[0] . '">';
+					$out .= '<button type="submit" name="wrc-entry-delete" value="' . $data[0] . '">Delete</button>';
+					$out .= '</form>';
+					return $out;
+			}
+			return '<b>' . var_export($data, true) . '</b>';
+		}
+	} // END class
 } // END if(!class_exists())
